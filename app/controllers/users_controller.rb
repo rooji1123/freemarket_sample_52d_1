f23567c887cbaf8cf255fc38beb3c11059ff4004
bbd <bbd.92@icloud.com>
2019-06-13 15:29:48 +0900

    AddMypage-AddRegistration
    add devise
    create users controller
    create devise views
    edit registration/new.html.haml
    add users resources to route

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.new
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
+  resources :users, only:[:show, :edit]
   resources :items
   resources :categories, only:[:index, :show]
-  resources :brans, only:[:index, :show]
+  resources :brands, only:[:index, :show]
   scope module: 'users' do
     resources :user_cards, only:[:edit, :update]
     resources :user_identifications, only:[:edit, :update]
